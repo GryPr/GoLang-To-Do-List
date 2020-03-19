b7d3923810249e20c44b1a269af0f0b79196571d
Kevin <promkev@gmail.com>
2020-03-19 19:29:05 -0400

    Start main.go

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetReportCaller(true)
+}
+
+// Health checks the health of the API
+func Health(w http.ResponseWriter, r *http.Request) {
+	log.Info("API Health is OK")                       // Logs to console API Health
+	w.Header().Set("Content-Type", "application/json") // Sets the content type to JSON
+	io.WriteString(w, `{"alive": true}`)               // Sends JSON
+}
+
 func main() {
+	log.Info("Starting API Server")
+	router := mux.NewRouter()
+	router.HandleFunc("/ping", Health).Methods("GET")
+	http.ListenAndServe(":8000", router)
 
 }
