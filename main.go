2ed1593fef0b5d7bf7fb535d65af4a10e74418bb
Kevin <promkev@gmail.com>
2020-03-19 21:21:02 -0400

    Use gorm for postgresql database

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"io"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	_ "github.com/lib/pq"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -19,12 +19,22 @@ const (
 	dbname   = "todo"
 )
 
+var db *gorm.DB
+var err error
+
+// ToDoItem struct contains the data of a to-do item
+type ToDoItem struct {
+	id          int `gorm:"primary_key"`
+	description string
+	completion  bool
+}
+
 func init() {
 	log.SetFormatter(&log.TextFormatter{})
 	log.SetReportCaller(true)
 }
 
-// Health checks the health of the API
+// Health returns the API health
 func Health(w http.ResponseWriter, r *http.Request) {
 	log.Info("API Health is OK")                       // Logs to console API Health
 	w.Header().Set("Content-Type", "application/json") // Sets the content type to JSON
@@ -36,16 +46,11 @@ func main() {
 
 	// Setting up database
 	psqlInfo := fmt.Sprintf("host=%s port =%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
 
 	// Setting up the router
 	router := mux.NewRouter()
