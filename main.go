9d8aaaaface71446cad386447a12e6b2d5988f4a
Kevin <promkev@gmail.com>
2020-03-19 20:38:53 -0400

    Set up postgresql

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"io"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
+)
+
+const (
+	host     = "127.0.0.1"
+	port     = 5432
+	user     = "postgres"
+	password = "gryphticon"
+	dbname   = "todo"
 )
 
 func init() {
@@ -17,13 +28,27 @@ func init() {
 func Health(w http.ResponseWriter, r *http.Request) {
 	log.Info("API Health is OK")                       // Logs to console API Health
 	w.Header().Set("Content-Type", "application/json") // Sets the content type to JSON
-	io.WriteString(w, `{"alive": true}`)               // Sends JSON
+	io.WriteString(w, `{"alive": true}`)               // JSON response to client
 }
 
 func main() {
 	log.Info("Starting API Server")
+
+	// Setting up database
+	psqlInfo := fmt.Sprintf("host=%s port =%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	// Setting up the router
 	router := mux.NewRouter()
 	router.HandleFunc("/ping", Health).Methods("GET")
 	http.ListenAndServe(":8000", router)
-
 }
